ed0ff4d1ec574b48aac9547f47f0a5ade37999fd
jcvillacobz <jucaviza6@gmail.com>
2022-11-12 14:35:32 -0500

    Listo para Deploy

--- src/app/components/gastos/listar-gastos/listar-gastos.component.ts
+++ src/app/components/gastos/listar-gastos/listar-gastos.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { PresupuestoService } from 'src/app/services/presupuesto.service';
 
 @Component({
@@ -6,15 +7,40 @@ import { PresupuestoService } from 'src/app/services/presupuesto.service';
   templateUrl: './listar-gastos.component.html',
   styleUrls: ['./listar-gastos.component.css']
 })
-export class ListarGastosComponent implements OnInit {
+export class ListarGastosComponent implements OnInit, OnDestroy {
+
+  subscription: Subscription;
+  presupuesto: number;
+  restante: number;
+  listGastos: any[] = [];
 
   constructor(private _presupuestoService: PresupuestoService) {
-    this._presupuestoService.getGastos().subscribe(data => {
+    this.presupuesto = 0;
+    this.restante = 0;
+    this.subscription = this._presupuestoService.getGastos().subscribe(data => {
       console.log(data);
+      this.restante = this.restante - data.cantidad;
+      this.listGastos.push(data);
     })
    }
 
   ngOnInit(): void {
+    this.presupuesto = this._presupuestoService.presupuesto;
+    this.restante = this._presupuestoService.restante;
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  agregarColor() {
+    if ((this.presupuesto / 4) > this.restante) {
+      return 'alert alert-danger';
+    } else if ((this.presupuesto / 2) > this.restante){
+      return 'alert alert-warning';
+    } else {
+      return 'alert alert-secondary';
+    }
   }
 
 }
